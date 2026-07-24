023c0bc0e94d48964f454788b2778847398d99ff
Paridhi Pandey <pandeyparidhi2006@gmail.com>
2026-07-24 13:12:20 +0530

    Add LeetCode 53 - Maximum Subarray

---
+++ Arrays/53_Maximum_Subarray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum = 0,maxSum = INT_MIN;
+        for(int val:nums)
+        {
+            currSum+=val;
+            maxSum = max(currSum,maxSum);
+            if(currSum <0){
+                currSum = 0;
+            }
+        }
+        return maxSum;
+
+        
+    }
+};
