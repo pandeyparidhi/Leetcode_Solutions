27138959c1dab59538c6e06cba3c0bc566fe7a02
Paridhi Pandey <pandeyparidhi2006@gmail.com>
2026-07-26 13:12:07 +0530

    Add LeetCode 217 - Contains Duplicate

---
+++ Arrays/217_Contains_Duplicate.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>s;
+        int n = nums.size();
+        for(int i = 0; i<n; i++){
+            if(s.find(nums[i]) != s.end()){
+                return true;
+                }
+                s.insert(nums[i]);
+            
+        }
+        return false;
+    }
+};
