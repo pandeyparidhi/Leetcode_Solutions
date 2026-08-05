bfb39a26202ce4057eda7eb5c55dc564efa86a36
Paridhi Pandey <pandeyparidhi2006@gmail.com>
2026-08-05 23:56:28 +0530

    Add LeetCode 1480 - Running Sum of 1D Array

---
+++ Arrays/1480_Running_Sum_of_1D_Array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum =0;
+        for(int i = 0; i<nums.size(); i++){
+            sum += nums[i];
+            nums[i] = sum;
+        }
+        return nums;
+    }
+};
