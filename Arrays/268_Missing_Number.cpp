2484f78461bcb45140f0d1257c542a2d0fe1ff2c
Paridhi Pandey <pandeyparidhi2006@gmail.com>
2026-08-24 22:05:58 +0530

    Add LeetCode 268 - Missing Number

---
+++ Arrays/268_Missing_Number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       
+        int n = nums.size();
+        int actualSum = 0;
+        int expectedSum = n * (n+1)/2;
+        for(int i = 0; i<n; i++){
+          actualSum += nums[i]; 
+        }
+        
+        return expectedSum - actualSum;
+    }
+};
