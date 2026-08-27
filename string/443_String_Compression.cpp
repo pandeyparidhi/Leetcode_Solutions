8d97c7a9aaf2e25faaf8e7fa2a6538d2ddbbfb3a
Paridhi Pandey <pandeyparidhi2006@gmail.com>
2026-08-27 11:35:21 +0530

    Add LeetCode 443 - String Compression

---
+++ string/443_String_Compression.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int idx = 0;
+        for(int i = 0; i<n; i++){
+            int count = 0;
+            char ch = chars[i];
+            while(i<n && ch == chars[i]){
+                count ++;
+                i++;
+            }
+            if(count == 1){
+                chars[idx++] = ch;
+            }
+            else{
+                chars[idx++] = ch;
+                string str = to_string(count);
+                for(char dig : str){
+                    chars[idx++] = dig;
+                }
+            }
+            i--;
+        }
+        chars.resize(idx);
+        return idx;
+
+
+        
+    }
+};
