1ca867db88ceb80db48a0ceb64f4a67ee92a3374
Paridhi Pandey <pandeyparidhi2006@gmail.com>
2026-09-02 21:16:31 +0530

    Add LeetCode 383 - Ransom Note

---
+++ string/383_Ransom_Note.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+     int freq[26] ={0};
+     for(char ch : magazine){
+        freq[ch - 'a']++;
+     }
+     for(char ch : ransomNote){
+        freq[ch -'a']--;
+        if(freq[ch - 'a'] < 0){
+            return false;
+        }
+     }
+     return true;
+        
+    }
+};
